16c515cc1a0fc6203f8077bf067995fc175f4cf0
Markus T <markus_t89@hotmail.com>
2018-03-23 19:51:19 +0100

    snart

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=PendingDeprecationWarning)
     import revpimodio2
-from time import sleep
+from time import sleep, time
 import threading
 from flask import Flask, render_template
 from flask_socketio import SocketIO
@@ -17,24 +17,49 @@ from copy import deepcopy
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
+def reassign(ordbok):
+    temp = {}
+    temp = ordbok
+    return temp
+
 
 class ventmotor:
     def __init__(self, go_up, go_down, up_switch, down_switch, io, db2, motor_name):
+        self.db2 = db2
         self.databas = db2['data']
         self.go_up = go_up
         self.go_down = go_down
         self.up_switch = up_switch
         self.down_switch = down_switch
         self.motor_name = motor_name
-        self.databas[motor_name]['maxposition'] = 100
-        self.databas[motor_name]['minposition'] = 0
-        self.databas[motor_name]['position'] = 0
         self.io = io
 
-    def testrange():
+    def testrange(self):
+        self.io.setoutput(self.go_down, True)
+        self.io.setoutput(self.go_up, False)
+        sleep(0.2)
+        while self.io.getoutput(self.down_switch) == True:
+            sleep(1)
+            logging.debug('Ventmotor: testrange: down while sleep 0.5 sec')
 
-        while down_switch == False:
-            self.databas[self.motor_name]['maxposition'] = 100
+        logging.debug('Ventmotor: testrange: while down exit')
+
+        self.io.setoutput(self.go_down, False) 
+        self.io.setoutput(self.go_up, True)
+        sleep(1)
+        start_time = time()
+        while self.io.getoutput(self.up_switch) == True:
+            sleep(1)
+            logging.debug('Ventmotor: testrange: up while sleep 0.5 sec')
+        
+        end_time = time()
+
+        self.io.setoutput(self.go_down, False) 
+        self.io.setoutput(self.go_up, False)
+
+        self.databas[self.motor_name]['ranger'] = abs(end_time - start_time)
+        self.db2 = reassign(self.db2)
+        transaction.commit()
 
     def moveabsoluteposition(self, position):
         if position > self.databas[self.motor_name]['position']:
@@ -52,7 +77,7 @@ class ventmotor:
             sleep(abs(movement))
             logging.debug('Set output %s False', self.go_down)
             self.io.setoutput(self.go_down, False)
-            
+
         databas_temp = self.databas[self.motor_name]
         databas_temp['position'] = position
         self.databas[self.motor_name] = databas_temp
@@ -69,19 +94,7 @@ class ventmotor:
 
 class io():
     def __init__(self):
-        self.revpi = revpimodio2.RevPiModIO(autorefresh=True)
-        #signal.signal(signal.SIGINT, self.exitfunktion)
-        #signal.signal(signal.SIGTERM, self.exitfunktion)
-        #self.revpi.io.Input.reg_event(self.eventfunktion)
-
-    def eventfunktion(self, ioname, iovalue):
-        #self.revpi.io.Output.value = iovalue
-        print(time.time(), ioname, iovalue)
-
-    def exitfunktion(self):
-        # Turn off LED A1 on the Core
-        print('Exiting')
-        self.revpi.core.A1 = revpimodio2.OFF
+        self.revpi = revpimodio2.RevPiModIO(autorefresh=False)
 
     def setoutput(self, output, value):
         if output == 'O_1':
@@ -92,10 +105,19 @@ class io():
             self.revpi.io.O_3.value = value
         elif output == 'O_4':
             self.revpi.io.O_4.value = value
-        sleep(1)
+        else:
+            return
+            
+        if value == True:
+            self.revpi.core.A1 = revpimodio2.RED
+        else:
+            self.revpi.core.A1 = revpimodio2.GREEN
+
+        self.revpi.writeprocimg()
 
 
     def getoutput(self, output):
+        self.revpi.readprocimg()
         if output == 'I_1':
             return self.revpi.io.I_1.value
         elif output == 'I_2':
@@ -104,10 +126,7 @@ class io():
             return self.revpi.io.I_3.value
         elif output == 'I_4':
             return self.revpi.io.I_4.value
-        sleep(1)
 
-    #def watch():
-    #    self.revpi.mainloop()
 
 class tempregulator():
     def __init__(self, factor=1):
@@ -135,13 +154,13 @@ class webserver():
         app.config['SECRET_KEY'] = 'secret!'
         socketio = SocketIO(app)
         self.socketio = socketio
+
         @app.route("/")
         def index():
             return render_template('index.html')
 
         @socketio.on('data_send', namespace='/test')
         def handle_client_connect_event(json):
-            temp = {}
             logging.debug('Webserver: Sparar JSON data: %s', json)
             print(self.db1['data'])
             for key in json:
@@ -150,12 +169,12 @@ class webserver():
                         self.db1['data'][key][key2] = json[key][key2]
                 else:
                     self.db1['data'][key] = json[key]
-            temp['data'] = self.db1['data']
-            self.db1['data'] = temp['data']
+            self.db1['data'] = reassign(self.db1['data'])
             transaction.commit()
 
         @socketio.on('connected', namespace='/test')
         def ccl(json):
+            self.socketio.emit('my_response', self.db1['data'], namespace='/test')
             logging.debug('Webserver: Ny anvandare ansluten: %s', json)
 
         @app.route('/static/<path:path>')
@@ -173,11 +192,10 @@ class webserver():
         self.db1_comp2['data'] = deepcopy(self.db2['data'])
     
         while True:
-            self.socketio.sleep(10)
+            self.socketio.sleep(1)
             if self.db1_comp2['data'] != self.db2['data']:
                 logging.debug('Webserver: skickar ny data till webbläsare')
-                self.socketio.emit('my_response', self.db2['data'],
-                                    namespace='/test')
+                self.socketio.emit('my_response', self.db2['data'], namespace='/test')
 
                 self.db1_comp2['data'] = deepcopy(self.db2['data'])
                 
@@ -192,26 +210,44 @@ class database():
     def newconn(self):
         return self.connection.root()
 
-class ventilationserver():
+class ventilationserver(threading.Thread):
     def __init__(self, db2):
+        threading.Thread.__init__(self)
+        self.shutdown_flag = threading.Event()
+        self.db2 = db2
         self.io = io()
-        #print(self.io.getoutput('I_1'))
-        motornord = ventmotor('O_1', 'O_2', 'I_1', 'I_2', self.io, db2, 'motornord')
-        motorsyd = ventmotor('O_3', 'O_4', 'I_3', 'I_4', self.io, db2, 'motorsyd')
-        
+
+    def run(self):
+        motornord = ventmotor('O_3', 'O_4', 'I_3', 'I_4', self.io, self.db2, 'motornord')
+        motorsyd = ventmotor('O_1', 'O_2', 'I_1', 'I_2', self.io, self.db2, 'motorsyd')
+
         regulator = tempregulator()
-        while 1:
-            if db2['data']['VentAutSwitch'] == True:
-                regulator.update(23, db2['data']['TempSetPointDay'])
-                print (regulator.correction)
-                print (regulator.direction)
+        while not self.shutdown_flag.is_set():
+            if self.db2['data']['motornord']['ranger'] == 0:
+                motornord.testrange()
+
+            if self.db2['data']['motorsyd']['ranger'] == 0:
+                motorsyd.testrange()
+
+            if self.db2['data']['VentAutSwitch'] == True:
+                regulator.update(23, self.db2['data']['TempSetPointDay'])
                 motornord.moverelativeposition(regulator.correction, regulator.direction)
                 logging.debug('Ventilatonserver: Automatiskt läge')
             else:
-                motornord.moveabsoluteposition(int(db2['data']['motornord']['movetoposition']))
-                motorsyd.moveabsoluteposition(int(db2['data']['motorsyd']['movetoposition']))
+                motornord.moveabsoluteposition(int(self.db2['data']['motornord']['movetoposition']))
+                motorsyd.moveabsoluteposition(int(self.db2['data']['motorsyd']['movetoposition']))
                 logging.debug('Ventilatonserver: Manuellt läge')
+
             sleep(3)
+
+        
+        print('Setting output to default values')
+        modio = revpimodio2.RevPiModIO(autorefresh=False)
+        modio.setdefaultvalues()
+        modio.writeprocimg()
+        
+        logging.debug('Ventilatonserver: Avslutar')
+
 
 def createdatabas(db):
     db['data'] = {  'TempSetPointDay': 20,
@@ -219,29 +255,47 @@ def createdatabas(db):
                     
                     'motorsyd': {    'position': 0, 
                                      'movetoposition': 0,
-                                     'minposition': 0, 
-                                     'maxposition': 0},
+                                     'ranger': 0},
 
                     'motornord': {   'position': 0, 
                                      'movetoposition': 0,
-                                     'minposition': 0, 
-                                     'maxposition': 0}
+                                     'ranger': 0}
               }
     transaction.commit()
 
+def signal_handler(signum, frame):
+    print('Caught signal %d' % signum)
+    raise serviceexit
+
+class serviceexit(Exception):
+    pass
+
 
 if __name__ == "__main__":
-    root = database()
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    try:
+        root = database()
     
-    ###Uncomment to create databas###
-    #db = root.newconn()
-    #createdatabas(db)
-    
-    webserver = threading.Thread(target=webserver, args=(root.newconn(),root.newconn(),))
-    ventilationserver = threading.Thread(target=ventilationserver, args=(root.newconn(),))
-    
-    webserver.start()
-    ventilationserver.start()
+        ###Uncomment to create databas###
+        #db = root.newconn()
+        #createdatabas(db)
+        
+        webserver = threading.Thread(target=webserver, args=(root.newconn(),root.newconn(),))
+        #ventilationserver = threading.Thread(target=ventilationserver, args=(root.newconn(),))
+        webserver.start()
+
+        ventilationserver = ventilationserver(root.newconn())
+        ventilationserver.start()
+
+        while True:
+            sleep(1)
+
+    except serviceexit:
+        ventilationserver.shutdown_flag.set()
+        ventilationserver.join()
+
 
     #db1 = root.newconn()
     #db1_temp = {}
